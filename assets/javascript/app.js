e5e856accb5f83f983085d621ad24bf054a2f141
Kgor <kevin.gorham@gmail.com>
2019-02-26 22:08:52 -0800

    scrambled brains

---
+++ assets/javascript/app.js
@@ -0,0 +1,54 @@
+// Computer selects and displays question at random
+// Create 4 buttons
+// if player clicks the correct answer
+// show congratulations screen for a few seconds before displaying the next question
+// if wrong 
+// show selected answer, highlight correct answer
+// if timer runs out diplay times up and highlight the correct answer
+// on final screen, show number of correct and incorrect answers, and an option to restart the game without reloading the page
+
+var questionArray = [{
+    question: "What is 2 x 2?",
+    choices: [2, 4, 8, "Threeve"],
+    correctAnswer: 1
+}],
+
+var triviaGame = {
+    // Game Variables
+    countdownTimer: 0,
+
+
+
+    // Game functions
+    startGameScreen: function() {
+
+    },
+
+    questionScreen: function() {
+
+    },
+
+    correctAnswerScreen: function() {
+
+    },
+
+    wrongAnswerScreen: function() {
+
+    },
+
+    timeOutScreen: function() {
+
+    },
+
+    populateAnswerButtons: function() {
+
+    },
+
+
+
+
+
+
+
+
+};
