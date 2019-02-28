95addf033ba6dc94c1e973a99f5997c5a7d61807
Kgor <kevin.gorham@gmail.com>
2019-02-28 10:02:02 -0800

    minor changes during office hours

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,55 +7,72 @@
 // if timer runs out diplay times up and highlight the correct answer
 // on final screen, show number of correct and incorrect answers, and an option to restart the game without reloading the page
 
-var questionArray = [{
-    "question": "What is 2 x 2?",
-    "choices": [2, 4, 8, "Threeve"],
-    "correctAnswer": 1
-}];
+
 
 var timer;
-var count = 3
+var count = 7
 // This triviaGame object will hold all game logic (include all variables time permitting)
 
 var triviaGame = {
     // Game Variables
     countdownTimer: 0,
     currentQuestionCounter: 0,
+    numCorrect: 0,
+    numIncorrect: 0,
 
     // Game function runs on click
     startGameScreen: function () {
         // clear start button -- could use help with a different method here
-        $("#start-div").remove();
+        $("#start-div").empty();
+        $("#count-num").html("<h1>Time Remaining: " + count + "</h1>")
         // call countDown function
-        triviaGame.countDownFunc();
+        // triviaGame.countDownFunc(); adding this functionailty to the questionScreen function
         triviaGame.questionScreen();
-        
+        // make buttons work -
+    // check if clicked button text is = correct answer 
+        // if so show result screen: Correct
+        // update correct/incorrect 
+    // else if clicked button text !== correct answer 
+        // show result screen: Incorrect!
+        // update correct/incorrect
+        $("button").on("click", function() {
+            if 
+
+        })
+       
+
+
     },
     countDownFunc: function () {
         console.log("countDownFunc running");
-        // create a timer counting down from 3 to 0 
+        // create a timer counting down from 3 to 0 -- removing this for the time being
         timer = setInterval(triviaGame.decrement, 1000);
-        // display timer
-        $("#count-num").html("<h1>" + count + "</h1>");
         // call questionScreen function at 0 
-
+        // triviaGame.questionScreen();
 
     },
     // function for timer decreasing
     decrement: function () {
         console.log("made it into decrement function");
         count--;
-        $("#count-num").html("<h1>" + count + "</h1>");
+        $("#count-num").html("<h1>Time Remaining: " + count + "</h1>");
         if (count <= 0) {
             console.log("made it to count === 0");
             clearInterval(timer);
+            // run a function to move to result screen with "out of time! displayed"
+            triviaGame.timeOutScreen();
+            // increcement incorrect answer variable 
+            triviaGame.numIncorrect++;
+
             // triviaGame.questionScreen(); moved this to line 31 but it's not working properly
         };
     },
 
     questionScreen: function () {
         console.log("We made it to the question screen!");
-        // Update timer text to "Time reamining: count"
+        // start count down
+        timer = setInterval(triviaGame.decrement, 1000);
+        // generate Q and A's
         triviaGame.populateQandA();
     },
 
@@ -67,13 +84,13 @@ var triviaGame = {
 
     // },
 
-    // timeOutScreen: function() {
+    timeOutScreen: function () {
+        console.log("out of time!")
+    },
 
-    // },
+    // resetScreen: function() {
 
-        // resetScreen: function() {
-
-        // }
+    // }
     // 
     populateQandA: function () {
         var questionArray = [{
@@ -84,9 +101,9 @@ var triviaGame = {
             question: "What is 2 - 2?",
             choices: [2, 4, 0, "Threeve"],
             correctAnswer: 2
-        }];  
+        }];
         // generate question
-        var displayQuestion = function() {
+        var displayQuestion = function () {
             console.log("displayQuestionFunction");
             $("#question-display").text(questionArray[triviaGame.currentQuestionCounter].question);
         };
@@ -107,3 +124,4 @@ var triviaGame = {
 
 // on click initiate questionScreen
 $(document).on("click", "#start-button", triviaGame.startGameScreen);
+
