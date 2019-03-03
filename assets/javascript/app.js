bb483b4f7d7c7134f27dba3b2e66f4d3f855f680
Kgor <kevin.gorham@gmail.com>
2019-03-03 00:00:33 -0800

    got restart game working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -65,7 +65,7 @@ var triviaGame = {
     },
 
     questionScreen: function () {
-        if (triviaGame.numCorrect + triviaGame.numIncorrect === 2) {
+        if (triviaGame.numCorrect + triviaGame.numIncorrect === 3) {
             triviaGame.gameOverScreen();
         } else {
             // start count down
